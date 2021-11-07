fa7d09f0a998824c42a449fb0f926be66d2c03e5
Serhii Riznyk <riznyk.serhii.v@gmail.com>
2021-11-07 11:38:50 +0200

    Code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   Button,
@@ -8,46 +8,52 @@ import {
   CardContent,
   Typography,
 } from "@material-ui/core";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "./state/store";
-
-import { interval } from "rxjs";
-import {
-  resetTime,
-  setIsActive,
-  setStart,
-  setTime,
-} from "./state/stopWatchSlice";
+import { interval, Subject, takeUntil } from "rxjs";
 
 function App() {
-  const { time, isActive, started } = useSelector(
-    (state: RootState) => state.stopWatch
-  );
-  const dispatch = useDispatch();
+  const [time, setTime] = useState(0);
+  const [isActive, setIsActive] = useState(false);
   useEffect(() => {
-    const observable = interval(10);
-    const subscription = observable.subscribe((x) => {
-      if (isActive && started) {
-        dispatch(setTime(10));
-      }
-    });
-    return () => subscription.unsubscribe();
-  }, [isActive, started]);
-
+    const unsubscribe = new Subject();
+    interval(1000)
+      .pipe(takeUntil(unsubscribe))
+      .subscribe(() => {
+        if (isActive) {
+          setTime((time) => time + 1);
+        }
+      });
+    return () => {
+      unsubscribe.next(false);
+      unsubscribe.complete();
+    };
+  }, [isActive]);
   const handleStart = (event: React.MouseEvent<HTMLButtonElement>) => {
-    dispatch(setIsActive(true));
-    dispatch(setStart(true));
+    setIsActive(true);
   };
   const handleStop = (event: React.MouseEvent<HTMLButtonElement>) => {
-    dispatch(setIsActive(false));
-    dispatch(setStart(false));
-    dispatch(resetTime());
+    if (time !== 0) {
+      setIsActive(false);
+      setTime(0);
+    }
   };
 
   const handleReset = (event: React.MouseEvent<HTMLButtonElement>) =>
-    dispatch(resetTime());
-  const handleWait = (event: React.MouseEvent<HTMLButtonElement>) =>
-    dispatch(setIsActive(false));
+    setTime(0);
+
+  let wasClicked = false;
+  let timeout: NodeJS.Timeout;
+  const handleWait = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (wasClicked) {
+      wasClicked = false;
+      clearTimeout(timeout);
+      setIsActive(false);
+      return;
+    }
+    wasClicked = true;
+    timeout = setTimeout(() => {
+      wasClicked = false;
+    }, 300);
+  };
 
   const timer = convertTime(time);
   return (
@@ -70,7 +76,8 @@ function App() {
               variant="contained"
               size="small"
               color="secondary"
-              onDoubleClick={handleWait}
+              // onDoubleClick={handleWait}
+              onClick={handleWait}
             >
               Wait
             </Button>
@@ -92,20 +99,8 @@ function App() {
 export default App;
 
 function convertTime(time: number) {
-  let pad = function (num: number, size: number) {
-    return ("000" + num).slice(size * -1);
-  };
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((time % (1000 * 60)) / 1000);
-  const milliseconds = Math.floor(time % (1000 * 60));
-  return (
-    pad(hours, 2) +
-    ":" +
-    pad(minutes, 2) +
-    ":" +
-    pad(seconds, 2) +
-    ":" +
-    pad(milliseconds, 3)
-  );
+  const hours = ("0" + Math.floor((time / 3600) % 60)).slice(-2);
+  const minutes = ("0" + Math.floor((time / 60) % 60)).slice(-2);
+  const seconds = ("0" + Math.floor(time % 60)).slice(-2);
+  return `${hours}:${minutes}:${seconds}`;
 }
